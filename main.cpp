568417c1b9cd9b25b81c49b8ddc27fde31018045
Ryan Seavey <seave067@d.umn.edu>
2024-09-13 20:13:49 -0500

    Commented out the parts that don't work

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@ int main() {
     std::cout << RyHash::hashNormal("dob") << '\n';
     std::cout << RyHash::hashNormal("Mikey") << '\n';
     std::cout << RyHash::hashNormal("dog") << '\n';
-    std::cout << "-- Divide --\n";
-    std::cout << RyHash::hash<std::string>("dog")<< '\n';
-    std::cout << RyHash::hash<std::string>("bog")<< '\n';
-    std::cout << RyHash::hash<std::string>("dog") << '\n';
+//    std::cout << "-- Divide --\n";
+//    std::cout << RyHash::hash<std::string>("dog")<< '\n';
+//    std::cout << RyHash::hash<std::string>("bog")<< '\n';
+//    std::cout << RyHash::hash<std::string>("dog") << '\n';
     return 0;
 }
