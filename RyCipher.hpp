5b0d3c72bd9fd9ebdc0c69bf9f9400cefa858a84
Ryan Seavey <seave067@d.umn.edu>
2024-09-18 13:18:52 -0500

    Made a version that works--albeit only in debug mode--but it does indeed encrypt and decrypt.

--- RyCipher.hpp
+++ RyCipher.hpp
@@ -14,28 +14,50 @@ class RyCipher {
 
 
 
+    static unsigned char *
+    Vigenere(unsigned char *evil_type, const unsigned char *evil_key, const size_t decSize, const size_t keySize, bool reverse);
 public:
-    template <class decType, class keyType>
-    [[nodiscard]] static std::string encode(decType encrypt_me, keyType key);
+
+
+    template <class encType, class keyType>
+    [[nodiscard]] static std::string encode(encType encrypt_me, keyType key);
 
 
     template <class decType, class keyType>
     [[nodiscard]] static decType decode(std::string cipherText, keyType key);
-
-
-
 };
 
-template<class decType, class keyType>
-decType RyCipher::decode(std::string cipherText, keyType key) {
-    auto * evil_key = static_cast<unsigned char*>(static_cast<void *>(key));
+template<class encType, class keyType>
+std::string RyCipher::encode(encType encrypt_me, keyType key) {
+    auto evil_type = static_cast<unsigned char*>(static_cast<void *>(encrypt_me.data())),
+           evil_key = static_cast<unsigned char*>(static_cast<void *>(key.data()));
 
-    return nullptr;
+    const size_t decSize = encrypt_me.size(), keySize = sizeof(keyType);
+
+    evil_type = Vigenere(evil_type, evil_key, decSize, keySize, false);
+
+
+    return {reinterpret_cast<const char*>(evil_type), decSize};
+}
+
+unsigned char * RyCipher::Vigenere(unsigned char *evil_type, const unsigned char *evil_key, const size_t decSize,
+                   const size_t keySize, bool reverse) {
+    for(size_t d = 0, k = 0; d < decSize; d++, ++k){
+        if(k >= keySize) k = 0;
+        if(not reverse) evil_type[d] -= evil_key[k] - VIG_OFFSET;
+        else evil_type[d] += evil_key[k] - VIG_OFFSET;
+    }
+    return evil_type;
 }
 
 template<class decType, class keyType>
-std::string RyCipher::encode(decType encrypt_me, keyType key) {
-    return {};
+decType RyCipher::decode(std::string cipherText, keyType key) {
+    auto evil_type = static_cast<unsigned char*>(static_cast<void *>(cipherText.data())),
+            evil_key = static_cast<unsigned char*>(static_cast<void *>(key.data()));
+    const size_t decSize = cipherText.size(), keySize = sizeof(keyType);
+
+    evil_type = Vigenere(evil_type, evil_key, decSize, keySize, true);
+    return reinterpret_cast<decType>(reinterpret_cast<void*>(evil_type));
 }
 
 
--- main.cpp
+++ main.cpp
@@ -6,11 +6,10 @@
 #define quick(T,arg) std::cout << RyHash::hash<T>(arg) << '\n'
 #define sassert(T,arg) quick(T,arg); assert((RyHash::hash<T>(arg) == RyHash::hash<T>(arg)))
 
-int main(int argc, char* argv[]) {
-    std::cout << RyHash::hash<std::string>("burrito") << '\n';
-    std::cout << RyHash::hash<std::string>("burrito") << '\n';
-    std::cout << RyHash::hash<std::string>("burrita") << '\n';
-
-    RyCipher::encode<std::string,std::string>("secret","key");
+int main() {
+    std::string secretText = RyCipher::encode<std::string,std::string>("super_secret","key");
+    std::cout << secretText << '\n';
+    std::string newText = RyCipher::decode<const char *, std::string>(secretText, "key");
+    std::cout << newText;
     return 0;
 }
